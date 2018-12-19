d286c7b71fbda6261f0a3914c8607b7b19feb143
Bryan Dodd <shhhhhitsasecret@donttell.com>
2018-12-19 09:15:16 -0500

    Add script that can be used to set defaults in Notes

---
+++ edit_me_mac_scripts.sh
@@ -0,0 +1,7 @@
+# Disable autocorrect in Notes
+#  This might work: Edit>Spelling and Grammar>Correct Spelling Automatically
+#  But if not, try running below
+# defaults write com.apple.notes NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable autocorrect in Notes
+# defaults delete com.apple.notes NSAutomaticSpellingCorrectionEnabled
